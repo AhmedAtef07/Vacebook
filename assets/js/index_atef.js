8475d76a4a18efd78fd074772903059894ae4a9a
AhmedAtef07 <ahmedatef07@hotmail.com>
2015-12-23 21:07:38 +0200

    Add about section to the left col

--- assets/js/index_atef.js
+++ assets/js/index_atef.js
@@ -1,4 +1,29 @@
 function runOnce() {
-  var cw = $('.circle-image').width();
-  $('.circle-image').css({'height':cw+'px'})
+  $('.circle-image').each(function() {
+    $(this).css({
+      height: $(this).width() + 'px'
+    });
+  });
+}
+
+function showInfoPoints(event) {
+  $(".info-point").first().show("fast", function showNext() {
+    $(this).next(".info-point").show("fast", showNext);
+  });
+}
+
+function hideInfoPoints(event) {
+  $(".info-point").last().hide("fast", function hidePrevious() {
+    $(this).prev(".info-point").hide("fast", hidePrevious);
+  });
+}
+
+function toggleInfoPoints(target) {
+  if (target.innerHTML === "Show More") {
+    showInfoPoints();
+    target.innerHTML = "Show Less";
+  } else {
+    hideInfoPoints();
+    target.innerHTML = "Show More";
+  }
 }
